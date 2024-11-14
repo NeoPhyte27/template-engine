e951d2079009baa215af23a405eedde8a092dd79
NeoPhyte27 <aldijinkpro27@gmail.com>
2024-11-14 15:24:32 +0700

    membuat router ke table

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
+Route::get('/table', function () {
+    return view('table', [
+        'title' => 'table'
+    ]);
 });
 
+Route::get('/data_tables', function () {
+    return view('data-tables', [
+        'title' => 'data table'
+    ]);
+});
